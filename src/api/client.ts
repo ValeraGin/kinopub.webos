37757ab443fb5d7d55f64b7c5e522bf8b8c06b50
Alexandr Dascal <alexandr.dascal@gmail.com>
2021-08-24 00:43:15 +0300

    Save tokend in cookies as alternative

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie';
+
 import storage from 'storage';
 
 import KinopubApiClient from './kinopub';
@@ -16,21 +18,25 @@ class ApiClient extends KinopubApiClient {
   }
 
   protected getAccessToken() {
-    return storage.getItem<string>('access_token');
+    return Cookies.get('access_token') || storage.getItem<string>('access_token');
   }
 
   protected getRefreshToken() {
-    return storage.getItem<string>('refresh_token');
+    return Cookies.get('refresh_token') || storage.getItem<string>('refresh_token');
   }
 
   protected saveTokens({ access_token, refresh_token, expires_in }: { access_token: string; refresh_token: string; expires_in: number }) {
     storage.setItem('is_logged', true, 30 * 24 * 3600);
     storage.setItem('access_token', access_token, expires_in);
     storage.setItem('refresh_token', refresh_token, 30 * 24 * 3600);
+
+    Cookies.set('access_token', access_token, { expires: expires_in / (24 * 3600) });
+    Cookies.set('refresh_token', refresh_token, { expires: 30 });
   }
 
   protected clearTokens() {
-    (['is_logged', 'access_token', 'refresh_token'] as const).forEach(storage.removeItem);
+    (['access_token', 'refresh_token'] as const).forEach((key) => Cookies.remove(key));
+    (['access_token', 'refresh_token', 'is_logged'] as const).forEach((key) => storage.removeItem(key));
   }
 }
 
